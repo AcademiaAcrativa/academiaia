695518fe647ce9437bfc060e10482c604d84aa46
AcademiaAcrativa <intermediadorapix@gmail.com>
2026-09-12 14:04:00 -0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+console.log(
+  '%cfeito pelo pedro luka crlh, veio procurar oq no console hein???',
+  'color: #00ffff; font-size: 16px; font-weight: bold; background: #111; padding: 8px 12px; border-radius: 6px; border: 1px solid #00c8ff;'
+);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
